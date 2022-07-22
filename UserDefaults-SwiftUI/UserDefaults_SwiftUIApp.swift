ecb8991914348f4c26eba4e186ac29600a54f1d8
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-21 23:19:02 -0700

    Initial Commit

---
+++ UserDefaults-SwiftUI/UserDefaults_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaults_SwiftUIApp.swift
+//  UserDefaults-SwiftUI
+//
+//  Created by Mitya Kim on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaults_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
